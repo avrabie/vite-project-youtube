e3efe04235c6b13dbe98ae0bc734b0e9a1a79f3d
moldo <net.vrabie@gmail.com>
2024-04-16 16:29:56 +0200

    trying to configure the proxy for preview

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/',
     plugins: [react()],
     preview: {
         port: 3000,
         host: true,
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'http://iaka-jobs-davide:8080',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '')
             }
